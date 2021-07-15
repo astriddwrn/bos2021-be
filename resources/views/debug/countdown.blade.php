3143bcc57e03096292ef11cfb1f687c737df945e
astriddwrn <70957853+astriddwrn@users.noreply.github.com>
2021-07-15 18:32:44 +0700

    Countdown rocket animation added

--- resources/views/debug/countdown.blade.php
+++ resources/views/debug/countdown.blade.php
@@ -12,11 +12,13 @@
     <link rel="stylesheet" href="{{ asset('css/countdown.css') }}">
 </head>
 <body class="font-sans">
+   
 
     <div class="w-screen h-screen bg-bgSpace relative">
+
         <div class="w-full h-full flex flex-col justify-center items-center relative z-10">
-            <div class="font-extrabold text-6xl text-center text-cWhite">Hi, Alexander Rainer</div>
-            <div class="relative my-12">
+            <div class="font-extrabold text-5xl text-center text-cWhite">Hi, Alexander Rainer</div>
+            <div class="relative mt-20 mb-8">
                 <div class="clock" id="clock1"></div>
                 <div class="flex flex-row justify-around text-center mt-2">
                     <label class="font-extrabold text-cWhite text-2xl font-sans">Days</label>
@@ -26,13 +28,13 @@
                 </div>
                 
             </div>
-            <div class="text-center font-extrabold text-4xl text-cWhite">left until BNCC Launching!</div>
-            <div class="text-center text-cWhite font-bold text-xl rounded p-2 bg-cLightBlue w-3/12 mt-20 hover:bg-cDarkerLightBlue transition duration-300 ease-in-out cursor-pointer">Back to Home</div>
+            <div class="text-center font-extrabold text-3xl text-cWhite">left until BNCC Launching!</div>
+            <div class="text-center text-cWhite font-bold text-xl rounded p-2 bg-cLightBlue w-3/12 mt-20 hover:bg-cDarkerLightBlue transition duration-300 ease-in-out cursor-pointergit">Back to Home</div>
         </div>
-        <img class="stars w-10/12 absolute z-0" src="{{url('./Asset/Image/countdown/bg-stars.svg')}}" alt="">
-        <img class="absolute bottom-0 z-0 w-full" src="{{url('./Asset/Image/countdown/bg-cloud1.svg')}}" alt="">
-        <img class="absolute bottom-5 z-0" src="{{url('./Asset/Image/countdown/bg-rocket.svg')}}" alt="">
-        <img class="absolute bottom-0 z-0 w-full" src="{{url('./Asset/Image/countdown/bg-cloud2.svg')}}" alt="">
+        <img class="stars w-10/12 absolute z-1" src="{{url('./Asset/Image/countdown/bg-stars.svg')}}" alt="">
+        <img class="absolute bottom-0 z-3 w-full" src="{{url('./Asset/Image/countdown/bg-cloud1.svg')}}" alt="">
+        <img class="rocket absolute z-4" src="{{url('./Asset/Image/countdown/bg-rocket.svg')}}" alt="">
+        <img class="absolute bottom-0 z-5 w-full" src="{{url('./Asset/Image/countdown/bg-cloud2.svg')}}" alt="">
     </div>
 
     <script src="{{url('./js/jquery-3.6.0.min.js')}}"></script>
