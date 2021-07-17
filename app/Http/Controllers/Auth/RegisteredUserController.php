8b6bde3041f7be73afe733a943ed85887c2ec95d
Gregory Hugo <hugo.gregory5@gmail.com>
2021-07-17 13:31:48 +0000

    Update Registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use App\Models\Schedule;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +21,8 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        $schedules = Schedule::all();
+        return view('auth.register', compact('schedules'));
     }
 
     /**
@@ -31,15 +33,41 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
+
     public function store(Request $request)
     {
+
+        $schedule = Schedule::find((int)$request->input('schedule_id'));
+        if($schedule->quota>0) {
+            $request->request->add(['isQuotaNotZero' => 1]);
+            // $request->request->add([1,'isQuotaNotZero']);
+        }
+        else {
+            $request->request->add(['isQuotaNotZero' => 0]);
+        }
+
+        // dd($request->all());
+
+        $message = [
+            'isQuotaNotZero.accepted' => 'Selected schedule is full, please select another one'
+        ];
+
         $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'whatsapp' => 'required|string',
-            'line_id' => 'required|string'
-        ]);
+            'whatsapp' => ['required','string','numeric','regex:/^(08)\d+/','digits_between:10,14'],
+            'line_id' => 'required|string',
+            'nim' => ['required','string','numeric','digits:10','regex:/^(23|24|25)\d+/','unique:users,nim'],
+            'campus' => 'required|string',
+            'major' => 'required|string',
+            'lnt_course' => 'required|string',
+            'schedule_id' => 'required',
+            'isQuotaNotZero' => 'accepted'
+        ], $message);
+
+        $schedule->quota--;
+        $schedule->save();
 
         $user = User::create([
             'name' => $request->name,
@@ -47,6 +75,11 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
             'whatsapp' => $request->whatsapp,
             'line_id' => $request->line_id,
+            'nim' => $request->nim,
+            'campus' => $request->campus,
+            'major' =>$request->major,
+            'lnt_course' => $request->lnt_course,
+            'schedule_id' => $request->schedule_id
         ]);
 
         event(new Registered($user));
