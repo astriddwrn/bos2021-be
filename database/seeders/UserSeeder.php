9cf41762a4263fe218330e12a0624108bd0b40cd
Gregory Hugo <hugo.gregory5@gmail.com>
2021-07-18 00:57:13 +0000

    User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'SCAC Binus',
+            'email' => 'scac.kemanggisan@binus.edu',
+            'password' => Hash::make('SCACxBNCC'),
+            'campus' => 'admin',
+            'major' => 'admin',
+            'whatsapp' => 'admin',
+            'line_id' => 'admin',
+            'lnt_course' => 'admin',
+            'nim' => 'admin',
+            'role' => 1,
+            'schedule_id' => 1
+        ]);
+
+        User::create([
+            'name' => 'Superadmin BNCC',
+            'email' => 'admin@bncc.net',
+            'password' => Hash::make('VIVABNCC!'),
+            'campus' => 'superadmin',
+            'major' => 'superadmin',
+            'whatsapp' => 'superadmin',
+            'line_id' => 'superadmin',
+            'lnt_course' => 'superadmin',
+            'nim' => 'superadmin',
+            'role' => 2,
+            'schedule_id' => 1
+        ]);
+
+        User::create([
+            'name' => 'Superadmin BNCC Malang',
+            'email' => 'adminmalang@bncc.net',
+            'password' => Hash::make('VIVA_BNCCMalang!'),
+            'campus' => 'superadmin malang',
+            'major' => 'superadmin malang',
+            'whatsapp' => 'superadmin malang',
+            'line_id' => 'superadmin malang',
+            'lnt_course' => 'superadmin malang',
+            'nim' => 'superadmin malang',
+            'role' => 3,
+            'schedule_id' => 1
+        ]);
+    }
+}
