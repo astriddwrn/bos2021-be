605f20cc2cd030431c2193fee23b729468049e7c
Gregory Hugo <hugo.gregory5@gmail.com>
2021-07-18 00:56:49 +0000

    Schedule Seeder

---
+++ database/seeders/ScheduleSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Schedule;
+
+class ScheduleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schedule::create([
+            'date' => '2020-08-24 09:20:00',
+            'quota' => 200,
+            'link1' => 'binus.zoom.us/',
+            'link2' => 'binus.zoom.us/'
+        ]);
+
+        Schedule::create([
+            'date' => '2020-08-25 11:20:00',
+            'quota' => 200,
+            'link1' => 'binus.zoom.us/',
+            'link2' => 'binus.zoom.us/'
+        ]);
+
+        Schedule::create([
+            'date' => '2020-08-26 13:20:00',
+            'quota' => 200,
+            'link1' => 'binus.zoom.us/',
+            'link2' => 'binus.zoom.us/'
+        ]);
+
+    }
+}
