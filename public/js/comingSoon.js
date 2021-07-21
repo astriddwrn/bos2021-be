e1eca61eb83e992e49e5e7b27947910a7adf51d8
astriddwrn <70957853+astriddwrn@users.noreply.github.com>
2021-07-21 23:48:49 +0700

    comingSoon On progress

---
+++ public/js/comingSoon.js
@@ -0,0 +1,10 @@
+// DATE UTC FORMAT (Year, Month-1, Date, Hour-5, Minutes, Seconds)
+var date = new Date(Date.UTC(2021, 7, 2, 8, 00, 60));
+var now = new Date();
+var diff = (date.getTime()/1000) - (now.getTime()/1000);
+
+
+var clock = $('.clock').FlipClock(diff,{
+      clockFace: 'DailyCounter',
+      countdown: true,
+});
