1d1418791c95dad40b5fd4d66c28fa6bbb74b97d
alfianzv <alfian101@gmail.com>
2021-07-29 18:18:41 +0700

    merged landing2 to landing

--- resources/views/debug/landing.blade.php
+++ resources/views/debug/landing.blade.php
@@ -126,6 +126,36 @@
         <h1 class="font-bold text-cBlackHome text-4xl text-center mt-14">And many more!</h1>
     </div>
 
+    {{-- Sponsor --}}
+    <div class="w-full font-sans my-32 px-72">
+        <h1 class="font-extrabold text-cDarkBlue text-5xl mb-14 text-center">Our Sponsors</h1>
+        <div class="rounded-xl border-2 border-cLightGray flex flex-col items-center">
+            <span class="text-base text-2xl font-medium -mt-5 bg-cWhite px-5">Gold Sponsor</span>
+            <div class="p-10">
+                <img class="h-52" src="{{ asset('Asset/Image/landing/sponsor-gold.svg')}}" alt="">
+            </div>
+        </div>
+        <div class="rounded-xl border-2 border-cLightGray flex flex-col items-center mt-14 ">
+            <span class="text-base text-2xl font-medium -mt-5 bg-cWhite px-5">Silver Sponsor</span>
+            <div class="p-10 flex flex-row justify-around w-full">
+                <img class="h-20" src="{{ asset('Asset/Image/landing/sponsor-silver1.svg')}}" alt="">
+                <img class="h-20" src="{{ asset('Asset/Image/landing/sponsor-silver2.svg')}}" alt="">
+            </div>
+        </div>
+        <div class="rounded-xl border-2 border-cLightGray flex flex-col items-center mt-14 ">
+            <span class="text-base text-2xl font-medium -mt-5 bg-cWhite px-5">Bronze Sponsor</span>
+            <div class="p-5 flex flex-row justify-around w-full flex-wrap">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze1.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze2.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze3.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze4.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze5.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze6.svg')}}" alt="">
+                <img class="h-16 m-5" src="{{ asset('Asset/Image/landing/sponsor-bronze7.svg')}}" alt="">
+            </div>
+        </div>
+    </div>
+
 
     <script src="{{url('./js/jquery-3.6.0.min.js')}}"></script>
     <script src="{{url('./js/landing.js')}}"></script>
