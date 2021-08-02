13645399afead99029708dde9453079d15bf26fc
astriddwrn <70957853+astriddwrn@users.noreply.github.com>
2021-08-02 09:23:02 +0700

    pop up removed

--- resources/views/debug/signup.blade.php
+++ resources/views/debug/signup.blade.php
@@ -17,13 +17,13 @@
         @endforeach
     -->
 
-    <div class="pop-up fixed success w-screen h-screen z-30">
+    <!-- <div class="pop-up fixed success w-screen h-screen z-30">
         <div class="fixed z-20 overlay w-screen h-screen bg-cBlack opacity-50"></div>
         <div class="modal fixed z-20 lg:w-6/12 w-96 h-3/6 bg-cWhite rounded-xl flex flex-col justify-center items-center px-20 text-center">
             <img class="w-30" src="{{ asset('Asset/Image/auth/success-icon.svg') }}" alt="">
             <div class="mt-7 font-medium text-2xl">Registration Success!</div>
         </div>
-    </div>
+    </div> -->
 
     <div class="flex flex-row font-sans h-full w-full">
         <div  class="main-container lg:static absolute lg:w-5/12 sm:w-8/12 w-11/12 lg:h-screen h-5/6 flex flex-col items-center z-10 bg-cWhite lg:rounded-none rounded-2xl " >
