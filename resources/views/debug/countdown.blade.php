ed895ca4b5a106f41721e4024c18656ab2d900c5
astriddwrn <70957853+astriddwrn@users.noreply.github.com>
2021-08-02 10:15:52 +0700

    countdown responsive done

--- resources/views/debug/countdown.blade.php
+++ resources/views/debug/countdown.blade.php
@@ -13,27 +13,30 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/flipclock.css') }}">
     <link rel="stylesheet" href="{{ asset('css/countdown.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/comingSoon.css') }}">
+
 </head>
 <body class="font-sans">
 
 
-    <div class="w-screen h-screen bg-bgSpace relative">
+    <div class="w-screen h-screen bg-bgSpace relative overflow-hidden">
 
-        <div class="w-full h-full flex flex-col justify-center items-center relative z-10">
-            <div class="font-extrabold text-5xl text-center text-cWhite font-sans">Hi, {{(Auth::user()->fullName)}}</div>
+        <div class="main-container w-full h-full flex flex-col sm:justify-center items-center relative z-10">
+            <div class="font-extrabold sm:text-5xl text-3xl text-center text-cWhite font-sans sm:mt-0 mt-10">Hi, {{(Auth::user()->fullName)}}</div>
+         
             <div class="relative mt-20 mb-8">
                 <div class="clock" id="clock1"></div>
-                <div class="flex flex-row justify-around text-center mt-2">
-                    <label class="font-extrabold text-cWhite text-2xl font-sans">Days</label>
-                    <label class="font-extrabold text-cWhite text-2xl font-sans">Hours</label>
-                    <label class="font-extrabold text-cWhite text-2xl font-sans">Minutes</label>
-                    <label class="font-extrabold text-cWhite text-2xl font-sans">Seconds</label>
+                <div class="time flex flex-row justify-around text-center mt-2">
+                    <label class="font-extrabold text-cWhite text-xl md:text-2xl font-sans">Days</label>
+                    <label class="font-extrabold text-cWhite text-xl md:text-2xl font-sans">Hours</label>
+                    <label class="font-extrabold text-cWhite text-xl md:text-2xl font-sans">Minutes</label>
+                    <label class="font-extrabold text-cWhite text-xl md:text-2xl font-sans sec">Seconds</label>
                 </div>
-
             </div>
-            <div class="text-center font-extrabold text-4xl text-cWhite">left until BNCC Launching!</div>
+         
+            <div class="text-center font-extrabold sm:text-4xl text-xl text-cWhite">left until BNCC Launching!</div>
 
-            <div class="text-center text-cWhite font-bold text-xl rounded p-2 bg-cLightBlue w-3/12 mt-20 hover:bg-cWhite hover:text-cLightBlue transition duration-300 ease-in-out cursor-pointer shadow-bsBtn"><a href="{{route('dashboard')}}">Return to Dashboard</a></div>
+            <div class="text-center text-cWhite font-bold sm:text-xl text-lg rounded p-2 bg-cLightBlue sm:w-96 w-64 sm:mt-20 mt-10 hover:bg-cWhite hover:text-cLightBlue transition duration-300 ease-in-out cursor-pointer shadow-bsBtn"><a href="{{route('dashboard')}}">Return to Dashboard</a></div>
 
         </div>
         <img class="stars w-10/12 absolute z-1" src="{{url('./Asset/Image/countdown/bg-stars.svg')}}" alt="">
