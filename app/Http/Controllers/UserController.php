12963df7067594697b8f87dffeaf990fc28ddca0
hugo.gregory5 <hugo.gregory5@gmail.com>
2021-08-03 08:52:58 +0700

    Fixed date for diff variables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,10 +42,10 @@ class UserController extends Controller
 
                 $diff_payment = ((new \DateTime($payment_appear))->diff(new \DateTime($now)));
 
-                $diff_reregis = ((new \DateTime('$schedules[0]->date'))->diff(new \DateTime($now)));
+                $diff_reregis = ((new \DateTime('2021-09-07 13:00:00'))->diff(new \DateTime($now)));
 
                 //Untuk disable change schedule kalau jadwal BL sudah mulai
-                $diff_change_schedule = ((new \DateTime('2021-08-31 16:08:00'))->diff(new \DateTime($now)));
+                $diff_change_schedule = ((new \DateTime($schedules[0]->date))->diff(new \DateTime($now)));
 
                 return view('userDashboard', compact('user','schedules','birthDate',
                                                         'diff_payment', 'diff_reregis',
