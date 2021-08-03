b1770f5e7a7b03841b5065128eebc0d3ac18ac88
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-08-03 18:20:00 +0700

    Update email.

--- resources/views/auth/newsletter_mail.blade.php
+++ resources/views/auth/newsletter_mail.blade.php
@@ -1,18 +1,18 @@
-Hola, Binusian 25! 👋
-<br /> <br />
-The wait is finally over! The registration for BNCC Launching has finally started, <b>NOW</b>!
-<br /> <br />
-Visit our <b>BNCC Expo</b> for knowing BNCC better in:
-🗓️: Every Saturdays in August (7, 14, 21, 28 August 2021)
-🕐: 14.00 - 15.00
-📍: <a href="https://bit.ly/MsTeamsChannel-BNCC">https://bit.ly/MsTeamsChannel-BNCC</a>
-<br /> <br />
-Or, you can visit us in our BNCC Channel in the MS Teams everyday for <b>Live Session QnA</b> in:
-🗓️: Every weekdays in August 2021
-🕐: 17.00 - 22.00 WIB
-📍: <a href="https://bit.ly/MsTeamsChannel-BNCC">https://bit.ly/MsTeamsChannel-BNCC</a>
-<br /> <br />
-Join our BNCC Launching, an event to meet our awesome alumni, explore what BNCC is and the procedures to become a proud BNCC member by clicking the link below!
-👉 <a href="https://launching.bncc.net/register">https://launching.bncc.net/register</a>
-<br /> <br />
+Hola, Binusian 25! 👋<br />
+<br />
+The wait is finally over! The registration for BNCC Launching has finally started, <b>NOW</b>!<br />
+<br />
+Visit our <b>BNCC Expo</b> for knowing BNCC better in:<br />
+🗓️: Every Saturdays in August (7, 14, 21, 28 August 2021)<br />
+🕐: 14.00 - 15.00<br />
+📍: <a href="https://bit.ly/MsTeamsChannel-BNCC">https://bit.ly/MsTeamsChannel-BNCC</a><br />
+<br />
+Or, you can visit us in our BNCC Channel in the MS Teams everyday for <b>Live Session QnA</b> in:<br />
+🗓️: Every weekdays in August 2021<br />
+🕐: 17.00 - 22.00 WIB<br />
+📍: <a href="https://bit.ly/MsTeamsChannel-BNCC">https://bit.ly/MsTeamsChannel-BNCC</a><br />
+<br />
+Join our BNCC Launching, an event to meet our awesome alumni, explore what BNCC is and the procedures to become a proud BNCC member by clicking the link below!<br />
+👉 <a href="https://launching.bncc.net/register">https://launching.bncc.net/register</a><br />
+<br />
 Invite your friends and don’t miss it out! See you soon!
