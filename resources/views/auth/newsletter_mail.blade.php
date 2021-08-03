fdcb1456b31b8975f9901a6c87823285f5611323
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-08-03 17:33:14 +0700

    Adding Mailing Newsletter Blasting.

---
+++ resources/views/auth/newsletter_mail.blade.php
@@ -0,0 +1,18 @@
+Hola, Binusian 25! 👋
+
+The wait is finally over! The registration for BNCC Launching has finally started, <b>NOW</b>!
+
+Visit our <b>BNCC Expo</b> for knowing BNCC better in:
+🗓️: Every Saturdays in August (7, 14, 21, 28 August 2021)
+🕐: 14.00 - 15.00
+📍: https://bit.ly/MsTeamsChannel-BNCC
+
+Or, you can visit us in our BNCC Channel in the MS Teams everyday for <b>Live Session QnA</b> in:
+🗓️: Every weekdays in August 2021
+🕐: 17.00 - 22.00 WIB
+📍: https://bit.ly/MsTeamsChannel-BNCC
+
+Join our BNCC Launching, an event to meet our awesome alumni, explore what BNCC is and the procedures to become a proud BNCC member by clicking the link below!
+👉 https://launching.bncc.net/register
+
+Invite your friends and don’t miss it out! See you soon!
