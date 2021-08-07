13f43dd5a0a2ef33108fea71be54ff969975a0b1
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-08-07 18:17:54 +0700

    Support download feature.

--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -19,10 +19,20 @@
                         </button>
                     </span>
                     @endif
-                    <button type="submit" class="flex flex-row justify-center items-center bg-cLightBlue hover:bg-cDarkerLightBlue text-cWhite font-semibold rounded px-4 py-2">
+                    <a href="@if ($region == 'ALL')
+                        {{route('download_all')}}
+                    @elseif ($region == 'KMG')
+                        {{route('download_KMG')}}
+                    @elseif ($region == 'ALS')
+                        {{route('download_ALS')}}
+                    @elseif ($region == 'BDG')
+                        {{route('download_BDG')}}
+                    @elseif ($region == 'MLG')
+                        {{route('download_MLG')}}
+                    @endif" class="flex flex-row justify-center items-center bg-cLightBlue hover:bg-cDarkerLightBlue text-cWhite font-semibold rounded px-4 py-2">
                         <img class="w-4 mr-2" src="{{ asset('Asset/Image/userDashboard/menu-download.svg')}}" alt="">
                         Download Participant Data
-                    </button>
+                    </a>
                 </div>
 
                 {{-- ALL REGION --}}
