0b470543787d8d27802c1cdca80373c0e780ece1
hugo.gregory5 <hugo.gregory5@gmail.com>
2021-08-28 22:24:41 +0700

    Guard for reregist

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -9,48 +9,48 @@ use Illuminate\Support\Facades\Auth;
 class MemberController extends Controller
 {
     public function create(Request $request){
-        $request->validate([
-            'lnt_course' => 'required',
-            'bnccid' => ['required','string','unique:members,bnccid','regex:/^(BNCC21)([0-9]{3})/'],
-            'linkedinUrl' => ['required','regex:/^(https:\/\/www.linkedin.com\/in\/)\w+/'],
-            'githubUrl' => ['required', 'regex:/^(https:\/\/github.com\/)\w+/'],
-            'ktp-upload' => 'required|mimes:jpg,jpeg,png|max:5480',
-            'fyp-upload' => 'required|mimes:jpg,jpeg,png|max:5480'
-        ] );
-        // dd($request->all());
-        // dd($request->user()->fullName);
-        $ktp = $request->file('ktp-upload');
-        $fn_ktp = $request->user()->fullName."_".$request->user()->nim."_ktp".time().".".$ktp->getClientOriginalExtension();
-        $ktp->move(public_path('ktp'),$fn_ktp);
+        if(Auth::user()->is_reregistered==0){
+            $request->validate([
+                'lnt_course' => 'required',
+                'bnccid' => ['required','string','unique:members,bnccid','regex:/^(BNCC21)([0-9]{3})/'],
+                'linkedinUrl' => ['required','regex:/^(https:\/\/www.linkedin.com\/in\/)\w+/'],
+                'githubUrl' => ['required', 'regex:/^(https:\/\/github.com\/)\w+/'],
+                'ktp-upload' => 'required|mimes:jpg,jpeg,png|max:5480',
+                'fyp-upload' => 'required|mimes:jpg,jpeg,png|max:5480'
+            ] );
 
-        $fyp = $request->file('fyp-upload');
-        $fn_fyp = $request->user()->fullName."_".$request->user()->nim."_fyp".time().".".$fyp->getClientOriginalExtension();
-        $fyp->move(public_path('fyp'),$fn_fyp);
+            $ktp = $request->file('ktp-upload');
+            $fn_ktp = $request->user()->fullName."_".$request->user()->nim."_ktp".time().".".$ktp->getClientOriginalExtension();
+            $ktp->move(public_path('ktp'),$fn_ktp);
 
-        Member::create([
-            'fullName' => $request->user()->fullName,
-            'email' => $request->user()->email,
-            'whatsapp' => $request->user()->whatsapp,
-            'line_id' => $request->user()->line_id,
-            'nim' => $request->user()->nim,
-            'campus' => $request->user()->campus,
-            'major' =>$request->user()->major,
-            'batch' => $request->user()->batch,
-            'gender' => $request->user()->gender,
-            'birthDate' => $request->user()->birthDate,
-            'placeBirth' => $request->user()->placeBirth,
-            'domicile' => $request->user()->domicile,
-            'address' => $request->user()->address,
+            $fyp = $request->file('fyp-upload');
+            $fn_fyp = $request->user()->fullName."_".$request->user()->nim."_fyp".time().".".$fyp->getClientOriginalExtension();
+            $fyp->move(public_path('fyp'),$fn_fyp);
 
-            'lnt_course' => $request->lnt_course,
-            'bnccid' => $request->bnccid,
-            'linkedinUrl' => $request->linkedinUrl,
-            'githubUrl' => $request->githubUrl,
-            'ktp-upload' => $fn_ktp,
-            'fyp-upload' => $fn_fyp
-        ]);
-        $request->user()->is_reregistered = 1;
-        $request->user()->save();
+            Member::create([
+                'fullName' => $request->user()->fullName,
+                'email' => $request->user()->email,
+                'whatsapp' => $request->user()->whatsapp,
+                'line_id' => $request->user()->line_id,
+                'nim' => $request->user()->nim,
+                'campus' => $request->user()->campus,
+                'major' =>$request->user()->major,
+                'batch' => $request->user()->batch,
+                'gender' => $request->user()->gender,
+                'birthDate' => $request->user()->birthDate,
+                'placeBirth' => $request->user()->placeBirth,
+                'domicile' => $request->user()->domicile,
+                'address' => $request->user()->address,
+                'lnt_course' => $request->lnt_course,
+                'bnccid' => $request->bnccid,
+                'linkedinUrl' => $request->linkedinUrl,
+                'githubUrl' => $request->githubUrl,
+                'ktp-upload' => $fn_ktp,
+                'fyp-upload' => $fn_fyp
+            ]);
+            $request->user()->is_reregistered = 1;
+            $request->user()->save();
+        }
 
         return back();
     }
@@ -97,4 +97,5 @@ class MemberController extends Controller
 
         return back();
     }
+
 }
